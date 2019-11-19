9ecc54761b3da071765844face707f82e8c5b491
nova togatorop <nova@roadbeats.com>
2019-11-19 12:23:40 +0100

    fix controller typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,34 @@
 
 puts "Cleans DB..."
+Dinosaur.destroy_all
 User.destroy_all
 
-puts "Adds 5 new User records..."
-5.times do
-  user = User.create(name: Faker::FunnyName.name,
+puts "Adds 10 new User records..."
+10.times do
+  user = User.create!(
+    name: Faker::FunnyName.name,
     password: 'valid_password',
     password_confirmation: 'valid_password',
-    email: Faker::Internet.email )
+    email: Faker::Internet.email
+  )
+
+  puts "Adds 5 new Dinosaur records..."
+  5.times do
+    dino = Dinosaur.create!(
+      name: Faker::FunnyName.name,
+      species: Faker::Games::Pokemon.name,
+      age: rand(1000),
+      gender: Faker::Gender.binary_type,
+      food: Faker::Food.vegetables,
+      habits: Faker::Lorem.words,
+      policy: Faker::Color.color_name,
+      location: Faker::Nation.capital_city,
+      description: Faker::Hacker.say_something_smart,
+      price: rand(1000),
+      user: user
+  )
+  end
 end
 
-puts "Adds 10 new Dinosaur records..."
-10.times do
-  user = Dinosaur.create(
-    name: Faker::FunnyName.name,
-    species: Faker::Games::Pokemon.name,
-    age: Faker::Number.number,
-    gender: Faker::Gender.binary_type,
-    food: Faker::Food.vegetables,
-    habits: Faker::Lorem.words,
-    policy: Faker::Color.color_name,
-    location: Faker::Nation.capital_city,
-    description: Faker::Hacker.say_something_smart,
-    price: Faker::Number.number,
-    user: User.last
-     )
-end
 
 
