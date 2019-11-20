f61f28c5004da644bd6e7c363ef318cd4d49cba1
Anja Laube <mail@anjalaube.com>
2019-11-20 15:57:15 +0100

    Homepage, My Account as CRUD User work, also added a link to logo in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:show, :edit, :update, :destroy] do
+  # resources :users, only: [:show, :edit, :update, :destroy] do
     resources :dinosaurs, only: [:index, :new, :create, :destroy, :show, :edit, :update]
-  end
+  # end
   get '/:id', to: 'pages#account', as: :account
+  # get 'user/:id', to: 'users#show', as: :user
 end
