c5773d2c587afc820b9cfe006eb570338cd8d362
CM <a.richert2111@icloud.com>
2019-11-20 12:06:35 +0100

    added the relationship to the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :dinosaurs
+  has_many :bookings
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
