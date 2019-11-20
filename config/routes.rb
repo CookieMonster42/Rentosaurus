93566a7f8d48fd614d6ba86a73980093e5976407
Stella Teichmann <stella.teichmann@outlook.de>
2019-11-20 14:32:20 +0100

    logo now links to the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :dinosaurs
   resources :dinosaurs, only: [:show] do
     resources :bookings, only: [:new, :create]
   end
-  resources :dinosaurs
   root to: 'pages#home'
 end
