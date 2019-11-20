2f6e1a4eb10243b5ec593ee6f9818fc37f836ddd
CM <a.richert2111@icloud.com>
2019-11-20 11:29:24 +0100

    Started the bookings table (routes: new, create)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,14 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
   end
+
+  def create
+    @booking = Booking.new(params[:id])
+    # this is for the pundit
+    authorize @booking
+
+    @booking.user = current_user
+    @booking.save
+    redirect_to @booking
+  end
 end
