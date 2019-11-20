57e49276ae63789c388ab473450b9e5d2832cb53
CM <a.richert2111@icloud.com>
2019-11-20 11:03:16 +0100

    fixed dino name validations

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -2,7 +2,6 @@ class Dinosaur < ApplicationRecord
   validates :name, presence: true
   belongs_to :user
   # validate that dinosaur needs a :name, :species, :age, :gender, :food, :price, :policy, :habits, :location
-  validates :name, presence: true
   # validates :species, presence: true
   # validates :age, presence: true
   # validates :gender, presence: true
