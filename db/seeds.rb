9b84645b35128adaf7a788a7d649a0123b0ecb70
nova togatorop <nova@roadbeats.com>
2019-12-20 23:05:21 +0100

    fix login form style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,74 @@
+require 'open-uri'
 
 puts "Cleans DB..."
 Booking.destroy_all
 Dinosaur.destroy_all
 User.destroy_all
 
-puts "Adds 5 new User records..."
-5.times do
-  user = User.create!(
-    name: Faker::FunnyName.name,
-    password: 'valid_password',
-    password_confirmation: 'valid_password',
-    email: Faker::Internet.email
+###########
+# user seed
+###########
+
+user1 = User.create({
+  name: 'Lala',
+  email: 'lala@example.com',
+  photo: 'lala.gif',
+  password: 123123
+})
+
+user2 = User.create({
+  name: 'Jane',
+  email: 'jane@example.com',
+  photo: 'jane.jpg',
+  password: 123123
+})
+
+puts "Users seeded! #{ User.count}"
+
+
+  dino = Dinosaur.new(
+    name: 'Inggrid',
+    species: 'Iguanodon',
+    age: 250,
+    gender: 'Female',
+    food: 'Fresh green leaves',
+    habits: 'Sleep under the stars',
+    location: 'Berlin',
+    description: 'No need breakfast.',
+    price: 550,
+    photo: 'https://i.ebayimg.com/images/g/CBMAAMXQuCdTfp4K/s-l300.jpg',
+    user: user1
   )
+  dino.save
 
-  puts "Adds 2 new Dinosaur records..."
-  2.times do
-    dino = Dinosaur.create!(
-      name: Faker::FunnyName.name,
-      species: Faker::Games::Pokemon.name,
-      age: rand(1000),
-      gender: Faker::Gender.binary_type,
-      food: Faker::Food.vegetables,
-      habits: Faker::Lorem.words,
-      policy: Faker::Color.color_name,
-      location: Faker::Nation.capital_city,
-      description: Faker::Hacker.say_something_smart,
-      price: rand(1000),
-      user: user
+  dino1 = Dinosaur.new(
+    name: 'Alice',
+    species: 'Allosaurus',
+    age: 250,
+    gender: 'Female',
+    food: 'Tofu',
+    habits: 'Drink milk at sunset',
+    location: 'Berlin',
+    description: 'Playful, easy to take care and love playing swing.',
+    price: 170,
+    photo: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c8b3ceb6-e094-45bb-a4e5-4048568bb2bc/d4i65ku-51b2bbdd-996f-44e8-8194-1935eaf1cec4.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi9jOGIzY2ViNi1lMDk0LTQ1YmItYTRlNS00MDQ4NTY4YmIyYmMvZDRpNjVrdS01MWIyYmJkZC05OTZmLTQ0ZTgtODE5NC0xOTM1ZWFmMWNlYzQuanBnIn1dXX0.4cXDhmUV9ItXV9ekwoOKJAG9N0gHwzWlp7RS7JYxL8Y',
+    user: user1
   )
-  end
-end
+  dino1.save
 
+  dino2 = Dinosaur.new(
+    name: 'Brontu',
+    species: 'Brontosaurus',
+    age: 250,
+    gender: 'Female',
+    food: 'Pickle',
+    habits: 'Read a book',
+    location: 'Berlin',
+    description: 'Love sleeping under sofa.',
+    price: 320,
+    photo: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c8b3ceb6-e094-45bb-a4e5-4048568bb2bc/d4i65ku-51b2bbdd-996f-44e8-8194-1935eaf1cec4.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi9jOGIzY2ViNi1lMDk0LTQ1YmItYTRlNS00MDQ4NTY4YmIyYmMvZDRpNjVrdS01MWIyYmJkZC05OTZmLTQ0ZTgtODE5NC0xOTM1ZWFmMWNlYzQuanBnIn1dXX0.4cXDhmUV9ItXV9ekwoOKJAG9N0gHwzWlp7RS7JYxL8Y',
+    user: user2
+  )
+  dino2.save
 
-
+puts "Dinos seeded! #{ Dinosaur.count}"
